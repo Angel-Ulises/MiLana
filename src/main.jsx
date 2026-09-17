9c9f505d9e5469549bf063f60e983eb388b209f5
Angel-Ulises <licangelulises@gmail.com>
2026-09-17 11:41:32 -0600

    Activa correcciones visuales y contacto en MiLana

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import SiteEnhancements from './siteEnhancements.jsx'
+import './site-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SiteEnhancements />
   </React.StrictMode>,
 )
